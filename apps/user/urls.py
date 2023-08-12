122b394b60412af9b34f1060e4000d065ba9ba67
andrejsajkin <kiwibon@yandex.ru>
2023-08-12 17:55:00 +0300

    попробуем отправлять запросы

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,4 +4,20 @@ __all__ = [
     "router",
 ]
 
+from sqlalchemy.orm import load_only
+
 router = APIRouter()
+
+from db.session import get_db_session
+from sqlalchemy import select
+from apps.user.models import User
+from db.session import AsyncSessionLocal
+
+
+async def get_all_users():
+    load_only(User.pk, User.first_name, User.email)
+    session = get_db_session()
+    select(User).filter(User.email == "first@kl.ru")
+    q = 5
+    async with AsyncSessionLocal() as session:
+        a = 5
--- db/session.py
+++ db/session.py
@@ -1,5 +1,16 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 
-with Session() as session:
-    with session.begin() as blok:
-        a = 5
+from conf import settings
+
+__all__ = [
+    "get_db_session",
+]
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(expire_on_commit=False, class_=AsyncSession, bind=engine)
+
+
+async def get_db_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
